84c57dc33e75fd8d94859ad0f9dbc4ccdf7fdb7c
Denis Cazzulo <denis.cazzulo@tesisquare.com>
2025-02-21 16:04:28 +0100

    SEO improvement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
+import robotsTxt from 'astro-robots-txt';
+import sitemap from 'astro-sitemap';
 
 export default defineConfig({
+  site: 'https://aboutdenis.eu',
+  integrations: [
+    robotsTxt(),
+    sitemap()
+  ],
 });
